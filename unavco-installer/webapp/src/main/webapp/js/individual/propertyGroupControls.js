0234598b2127c8291616552820095aeba051fc7e
Benjamin Gross <mbgross@unavco.org>
2016-02-24 17:04:06 -0700

    tweak to tab controls

--- unavco-installer/webapp/src/main/webapp/js/individual/propertyGroupControls.js
+++ unavco-installer/webapp/src/main/webapp/js/individual/propertyGroupControls.js
@@ -55,20 +55,21 @@ $(document).ready(function(){
 
     
     function removeHash () { 
-      //window.location.hash = "";
-        var scrollV, scrollH, loc = window.location;
-        if ("pushState" in history)
-            history.replaceState("", document.title, loc.pathname + loc.search);
-        else {
-            // Prevent scrolling by storing the page's current scroll offset
-            scrollV = document.body.scrollTop;
-            scrollH = document.body.scrollLeft;
+        if ( location.hash ) {
+            var scrollV, scrollH, loc = window.location;
+            if ("pushState" in history)
+                history.replaceState("", document.title, loc.pathname + loc.search);
+            else {
+                // Prevent scrolling by storing the page's current scroll offset
+                scrollV = document.body.scrollTop;
+                scrollH = document.body.scrollLeft;
 
-            loc.hash = "";
+                loc.hash = "";
 
-            // Restore the scroll offset, should be flicker free
-            document.body.scrollTop = scrollV;
-            document.body.scrollLeft = scrollH;
+                // Restore the scroll offset, should be flicker free
+                document.body.scrollTop = scrollV;
+                document.body.scrollLeft = scrollH;
+            }
         }
     }
     
